81c2a82c7084e8565bd0d9526f7b5fa05bc68a4b
Vikas-Singh-IITR <vsingh@ar.iitr.ac.in>
2021-07-21 14:34:57 +0530

    hashed password

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Task = require("../models/task");
+// const { update } = require("../models/User");
 
 const router = new express.Router();
 
@@ -48,10 +49,16 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
+    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+
+    const task = await Task.findById(req.params.id);
+    updates.forEach((update) => {
+      task[update] = req.body[update];
     });
+    await task.save();
 
     if (!task) {
       return res.status(404).send();
