9f90a061d51fd358bd0fe7f8aaa0123d95f045e6
Vikas-Singh-IITR <vsingh@ar.iitr.ac.in>
2021-07-21 13:02:44 +0530

    CRUD for user and task

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age can not be negative");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please enter a valid email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("please use stronger password");
+      }
+    },
+  },
+});
+
+module.exports = User;
